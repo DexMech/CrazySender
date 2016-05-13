3f977805ad049d7b66e8fde8c22108db1908dc38
alex <alex.junior.carlos.23@gmail.com>
2016-05-13 17:09:08 -0300

    primeiro commit

---
+++ src/Teste.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class Teste {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+
+		SSLSocketFactory factory=(SSLSocketFactory) SSLSocketFactory.getDefault();
+		SSLSocket sslsocket=(SSLSocket) factory.createSocket("smtp.gmail.com",465);
+
+		InputStreamReader le = new InputStreamReader(sslsocket.getInputStream());
+		BufferedReader lei = new BufferedReader(le);
+		OutputStreamWriter out = new OutputStreamWriter(sslsocket.getOutputStream(),"UTF-8");
+		out.write("EHLO smtp.gmail.com \r\n");
+		out.flush();
+		
+		
+		Thread.sleep(1000);
+		out.write("AUTH LOGIN \r\n");
+		out.flush();
+		Thread.sleep(1000);
+		out.write("YWxleC5qdW5pb3IuY2FybG9zMjNAZ21haWwuY29tCg==\r\n");
+		out.flush();
+		out.write("VG9yaW1lTmFndW1lMDAxMAo=\r\n");
+		out.flush();
+		out.write("MAIL FROM: <alex.junior.carlos23@gmail.com> \r\n");
+		out.flush();
+		Thread.sleep(1000);
+		out.write("RCPT TO: <willoucassiano@gmail.com> \r\n");
+		out.flush();
+		Thread.sleep(1000);
+		out.write("DATA \r\n");
+		out.flush();
+		out.write("Content-Type: text/plain;charset=utf-8\r\n");
+		out.flush();
+		
+		out.write("Esse email é um teste");
+		out.flush();
+		out.write("\r\n.\r\n");
+		out.flush();
+		Thread.sleep(1000);
+		sslsocket.close();
+		lei.close();
+		le.close();
+		System.out.println("deu certo");
+
+
+	}
+
+}
